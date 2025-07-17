cff1b02f37f31c6ac255a91823f2bf1ebb1ed3ed
João Pedro Lage <JoaoPedro.Lage@outlook.com>
2025-07-17 10:10:30 -0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+// webpack.config.js (na raiz do projeto)
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  
+  // Configura polyfills para módulos Node.js
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer/'),
+  };
+
+  return config;
+};
